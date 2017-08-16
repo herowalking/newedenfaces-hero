81b745adfc7485a69e7c3a59c14da043d3b2ea42
Lincoln <lincolnpb@hotmail.com>
2017-08-16 16:51:11 +0800

    update server.js

--- server.js
+++ server.js
@@ -1,31 +1,39 @@
-var swig  = require('swig');
-var React = require('react');
-var Router = require('react-router');
-var routes = require('./app/routes');
+require('babel-register');
 
-var express = require('express');
 var path = require('path');
-var logger = require('morgan');
+var express = require('express');
 var bodyParser = require('body-parser');
+var compression = require('compression');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
 var async = require('async');
+var colors = require('colors');
 var mongoose = require('mongoose');
 var request = require('request');
+var React = require('react');
+var ReactDOM = require('react-dom/server');
+var Router = require('react-router');
+var swig  = require('swig');
+var xml2js = require('xml2js');
 var _ = require('underscore');
 
 var config = require('./config');
+var routes = require('./app/routes');
 var Character = require('./models/character');
 
 var app = express();
 
 mongoose.connect(config.database);
 mongoose.connection.on('error', function() {
-    console.info('Error: Could not connect to MongoDB. Did you forget to run `mongod`?');
+    console.info('Error: Could not connect to MongoDB. Did you forget to run `mongod`?'.red);
 });
 
 app.set('port', process.env.PORT || 8090);
+app.use(compression());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 /**
